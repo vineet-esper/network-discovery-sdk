fd6fef7654dcfdc617e1dc85360886b266fe72f2
vineet-esper <142732223+vineet-esper@users.noreply.github.com>
2024-10-02 14:33:09 +0530

    improved logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from network_discovery_sdk import NetworkDiscoverySdk
 import time
+import logging
 
 # Create an instance of NetworkDiscoverySdk
 sdk = NetworkDiscoverySdk("edge-ai-service", 8000)
@@ -8,16 +9,16 @@ try:
     # Register the service
     sdk.register()
 
-    print("Service registered. Press Ctrl+C to stop.")
+    logging.info("Service registered. Press Ctrl+C to stop.")
     
     # Keep the service running
     while True:
         time.sleep(1)
 
 except KeyboardInterrupt:
-    print("\nStopping the service...")
+    logging.info("\nStopping the service...")
 
 finally:
     # Unregister the service
     sdk.unregister()
-    print("Service unregistered.")
+    logging.info("Service unregistered.")
